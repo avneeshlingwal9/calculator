fbc129f3adc7036b77f8656cf0f0d428a749b81d
avneeshlingwal9 <avaneeshlingwal@gmail.com>
2025-06-22 17:51:12 +0530

    Complete functionalities

--- script.js
+++ script.js
@@ -7,9 +7,12 @@ const calculate = document.querySelector(".calculate");
 
 const clear = document.querySelector(".clear");
 
+const dlt = document.querySelector(".delete");
+
 let expr = "";
 
 let oprlist = [];
+
 
 
 function isOperator(el){
@@ -33,7 +36,7 @@ const divide = function (a , b){
     if(b === 0){
         return "Not here. "
     }
-    return Math.floor(a/b); 
+    return (a/b).toPrecision(4);
 }
 
 
@@ -58,17 +61,10 @@ function operate(num1 , num2 , operator){
 
 }
 
+function numberProcessing(e){
+    let lastInserted = expr[expr.length - 1]; 
 
-
-numList.forEach(
-    (element) => {
-
-        element.addEventListener("click", (e)=>{
-
-
-            let lastInserted = expr[expr.length - 1]; 
-
-            if(isOperator(lastInserted)){
+            if(isOperator(lastInserted) || expr.length === 0){
                 display.textContent ="";
             }
             display.textContent += e.target.textContent;
@@ -79,12 +75,13 @@ numList.forEach(
 
 
 
+}
 
-        }
-        
-    )
-        
-    }
+numList.forEach(
+    (element) => {
+
+        element.addEventListener("click", numberProcessing)
+}
 
 
 );
@@ -134,6 +131,9 @@ function calcuateExpr(){
     if(expr[i] === "-"){
         num1 += expr[i++];
     }
+    if(expr[i] === "+"){
+        i++; 
+    }
     while(i < expr.length && !isOperator(expr[i])){
 
         num1 += expr[i++];
@@ -159,7 +159,8 @@ function calcuateExpr(){
 
     } */
     
-
+    if(num1.length != 0 && num2.length != 0 && isOperator(operator)){
+        
         num1 = Number(num1);
         num2 = Number(num2); 
 
@@ -169,6 +170,22 @@ function calcuateExpr(){
 
         expr = display.textContent + expr; 
 
+        let onlyResult = true; 
+
+        oprlist.forEach((el) => {
+
+            if(expr.includes(el)){
+
+                onlyResult = false; 
+
+            }
+
+        })
+
+        if(onlyResult){
+            expr = ""; 
+        }
+    }
 
 
 
@@ -185,10 +202,83 @@ function calcuateExpr(){
 
 }
 
+function backClear(){
+       expr = expr.substring(0 , expr.length - 1); 
+
+    display.textContent = display.textContent.slice(0 ,- 1); 
+}
+
 clear.addEventListener("click" , ()=> 
     {expr = "" , display.textContent = 0}
 );
 
+dlt.addEventListener("click" , backClear);
 
+/// Keyboard Input
+document.addEventListener("keydown" , (e)=>{
+
+    console.log(e); 
+
+
+    let nums = "0123456789";
+
+
+
+    if(nums.includes(e.key)){
+
+        let lastInserted = expr[expr.length - 1]; 
+
+            if(isOperator(lastInserted) || expr.length === 0){
+                display.textContent ="";
+            }
+            display.textContent += e.key;
+
+            expr += e.key;
+
+            console.log(expr);
+
+        
+
+    }
+    if(oprlist.includes(e.key)){
+
+        display.textContent = ""; 
+        display.textContent += e.key;
+        let lastInserted = expr[expr.length - 1]; 
+        if(isOperator(lastInserted)){
+
+            expr = expr.replace(lastInserted , e.key);
+            
+
+        }
+        else{
+
+            expr += e.key; 
+
+
+
+        }
+
+
+
+    }
+    if(e.key === "Enter"){
+        calcuateExpr();
+    }
+
+    if(e.key === "Backspace"){
+
+        backClear();
+
+
+
+
+    }
+
+
+
+
+
+})
 
 
